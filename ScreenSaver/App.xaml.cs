b78fbfce50ffbbdd47f80bb216e8e1d37e86b530
闫驚鏵 <yanjinhuamail@163.com>
2022-07-10 16:15:47 +0800

    add in code.

---
+++ ScreenSaver/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Application = System.Windows.Application;
+
+namespace ScreenSaver
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private HwndSource winWPFContent;
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0 || e.Args[0].ToLower().StartsWith("/s"))
+            {
+                foreach (Screen s in Screen.AllScreens)
+                {
+                    var window = new MainWindow();
+                    window.Left = s.WorkingArea.Left;
+                    window.Top = s.WorkingArea.Top;
+                    window.Width = s.WorkingArea.Width;
+                    window.Height = s.WorkingArea.Height;
+                    window.Show();
+                }
+            }
+            else if (e.Args[0].ToLower().StartsWith("/p"))
+            {
+                var window = new MainWindow();
+                var previewHandle = Convert.ToInt32(e.Args[1]);
+                var pPreviewHnd = new IntPtr(previewHandle);
+                var lpRect = new RECT();
+                var bGetRect = Win32API.GetClientRect(pPreviewHnd, ref lpRect);
+                var sourceParams = new HwndSourceParameters("sourceParams");
+                sourceParams.PositionX = 0;
+                sourceParams.PositionY = 0;
+                sourceParams.Height = lpRect.Bottom - lpRect.Top;
+                sourceParams.Width = lpRect.Right - lpRect.Left;
+                sourceParams.ParentWindow = pPreviewHnd;
+                sourceParams.WindowStyle = (int)(WindowStyles.WS_VISIBLE | WindowStyles.WS_CHILD | WindowStyles.WS_CLIPCHILDREN);
+                winWPFContent = new HwndSource(sourceParams);
+                winWPFContent.Disposed += (o, args) => window.Close();
+                winWPFContent.RootVisual = window.MainGrid;
+            }
+
+        }
+    }
+}
---
+++ ScreenSaver/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ScreenSaver
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty stringCollectionProperty =
+            DependencyProperty.Register("stringCollection", typeof(ObservableCollection<string>), typeof(MainWindow),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty HourProperty =
+            DependencyProperty.Register("Hour", typeof(string), typeof(MainWindow), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty MinuteProperty =
+            DependencyProperty.Register("Minute", typeof(string), typeof(MainWindow), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty SecondProperty =
+            DependencyProperty.Register("Second", typeof(string), typeof(MainWindow), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register("Date", typeof(string), typeof(MainWindow), new PropertyMetadata());
+
+        private readonly DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += delegate
+            {
+                WindowState = WindowState.Maximized;
+                Mouse.OverrideCursor = Cursors.None;
+                var date = DateTime.Now;
+                Hour = date.ToString("HH");
+                Minute = date.ToString("mm");
+                Date =
+                    $"{date.Month} / {date.Day}   {CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(date.DayOfWeek)}";
+                stringCollection = new ObservableCollection<string>();
+                var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+                var directoryInfo = new DirectoryInfo(path);
+                foreach (var item in directoryInfo.GetFiles())
+                {
+                    if (Path.GetExtension(item.Name) != ".jpg") continue;
+                    stringCollection.Add(item.FullName);
+                }
+
+                timer.Interval = TimeSpan.FromSeconds(1);
+                timer.Tick += delegate
+                {
+                    date = DateTime.Now;
+                    Hour = date.ToString("HH");
+                    Minute = date.ToString("mm");
+                    Date =
+                        $"{date.Month} / {date.Day}   {CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(date.DayOfWeek)}";
+                };
+                timer.Start();
+            };
+            MouseDown += delegate { Application.Current.Shutdown(); };
+            KeyDown += delegate { Application.Current.Shutdown(); };
+        }
+
+        public ObservableCollection<string> stringCollection
+        {
+            get => (ObservableCollection<string>)GetValue(stringCollectionProperty);
+            set => SetValue(stringCollectionProperty, value);
+        }
+
+
+        public string Hour
+        {
+            get => (string)GetValue(HourProperty);
+            set => SetValue(HourProperty, value);
+        }
+
+        public string Minute
+        {
+            get => (string)GetValue(MinuteProperty);
+            set => SetValue(MinuteProperty, value);
+        }
+
+        public string Second
+        {
+            get => (string)GetValue(SecondProperty);
+            set => SetValue(SecondProperty, value);
+        }
+
+
+        public string Date
+        {
+            get => (string)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+       
+    }
+}
